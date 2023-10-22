93910542571773de0104c9516002516255ff4d99
cjmzone <cjm.zone@gmail.com>
2023-10-22 10:23:27 -0700

    First Commit

---
+++ prog1/solitaire.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Solitaire 4ever" << endl;
+	return 0;
+	}
---
+++ prog3/mysort.cpp
@@ -0,0 +1,68 @@
+// Connor McKenna
+// (CECS 325-01)
+// Program 3 - Sorting Contest
+// Due Date (10/9/2023)
+// I certify that this program is my own original work. I did not copy any part of this program from
+// any other source. I further certify that I typed each and every line of code in this program.
+
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::ios;
+
+int main(int count, char *file[]){
+
+        if (count !=3){
+                cout << "mysort accepts 2 paramenters only!" << endl;
+                cout << "P1: input file" << endl;
+                cout << "P2: output file" << endl;
+                return 1;
+        }
+
+        // Input file
+        ifstream fileIn;
+        fileIn.open(file[1]);
+
+        // Output file
+        ofstream fileOut;
+        fileOut.open(file[2], ios::out | ios::trunc);
+
+        int size = 0; 
+        int num;
+        // get the size 
+        while(fileIn >> num){
+                size++;
+        }
+        // reset fileIn
+        fileIn.close();
+        fileIn.open(file[1]);
+
+        int * r_numbers= new int[size];
+
+        for(int i = 0; i < size; i++) {
+                fileIn >> r_numbers[i];
+        }
+        // bubble sort
+        for(int i = 0; i < size; i++){
+                for(int j = i + 1; j < size; j++) {
+                        if(r_numbers[j] < r_numbers[i]){
+                                int temp = r_numbers[i];
+                                r_numbers[i] = r_numbers[j];
+                                r_numbers[j] = temp;
+                        }
+                }
+        }
+
+        for(int i = 0; i < size; i++) {
+                fileOut << r_numbers[i] << endl;
+        }
+
+        delete []r_numbers; 
+        fileIn.close();
+        fileOut.close();
+        return 0;
+}
